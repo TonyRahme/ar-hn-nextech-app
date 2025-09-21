09c000145d10c1c889290edb70681ce36aaa6c82
TonyRahme <59179569+TonyRahme@users.noreply.github.com>
2025-09-21 16:40:28 -0400

    feat(comment-dialog): scaffold component dialog

---
+++ client/src/app/components/comments-dialog/comments-dialog.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-comments-dialog',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './comments-dialog.component.html',
+  styleUrls: ['./comments-dialog.component.scss']
+})
+export class CommentsDialogComponent {
+
+}
