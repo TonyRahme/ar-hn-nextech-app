cb233ef003892ddcf8041e54724405a25c932a9a
TonyRahme <59179569+TonyRahme@users.noreply.github.com>
2025-09-21 16:55:22 -0400

    fix(hn-api.service): import the correct environment file

--- client/src/app/services/hn-api.service.ts
+++ client/src/app/services/hn-api.service.ts
@@ -2,7 +2,7 @@ import { EventEmitter, Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { ItemDto, PagedResult } from '../models/hackerNews.model';
 import { Observable } from 'rxjs';
-import { environment } from 'environments/environment.prod';
+import { environment } from 'environments/environment';
 
 @Injectable({
   providedIn: 'root',
